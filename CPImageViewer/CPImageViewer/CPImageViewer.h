e00cb149746e8685cd86090cbb5f74d1f2a93884
Jose Torres <torrescardenasjose@gmail.com>
2017-01-17 22:42:00 -0500

    Added new base project and cocoa framework.

---
+++ CPImageViewer/CPImageViewer/CPImageViewer.h
@@ -0,0 +1,19 @@
+//
+//  CPImageViewer.h
+//  CPImageViewer
+//
+//  Created by Jose Torres on 17/1/17.
+//  Copyright © 2017 Jose Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CPImageViewer.
+FOUNDATION_EXPORT double CPImageViewerVersionNumber;
+
+//! Project version string for CPImageViewer.
+FOUNDATION_EXPORT const unsigned char CPImageViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPImageViewer/PublicHeader.h>
+
+
