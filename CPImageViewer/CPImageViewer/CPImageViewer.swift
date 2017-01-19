23b2ccdaaae751896e8ff271c38a4467d2e0240b
Jose Torres <torrescardenasjose@gmail.com>
2017-01-18 21:50:40 -0500

    Created CPImageViewer class in framework.

---
+++ CPImageViewer/CPImageViewer/CPImageViewer.swift
@@ -0,0 +1,19 @@
+//
+//  CPImageViewer.swift
+//  CPImageViewer
+//
+//  Created by Jose Torres on 17/1/17.
+//  Copyright © 2017 Jose Torres. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    public func sayHi() {
+        
+        print("hola coche")
+    }
+    
+}
--- CPImageViewerDemo/ViewController.swift
+++ CPImageViewerDemo/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import CPImageViewer
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let temp = UIImageView()
+        temp.sayHi()
     }
 
     override func didReceiveMemoryWarning() {
