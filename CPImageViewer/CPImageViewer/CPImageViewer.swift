ecb67c2e83a2811b610264f6d1d7505230ca4c86
Jose Torres <torrescardenasjose@gmail.com>
2017-01-18 22:06:48 -0500

    Base implementation for CPImageView.

--- CPImageViewer/CPImageViewer/CPImageViewer.swift
+++ CPImageViewer/CPImageViewer/CPImageViewer.swift
@@ -11,9 +11,50 @@ import UIKit
 
 extension UIImageView {
     
-    public func sayHi() {
+}
+
+final public class CPImageView: UIImageView, NSCopying {
+    
+    @IBOutlet public var parentViewController: UIViewController?
+    
+    public func copy(with zone: NSZone? = nil) -> Any {
         
-        print("hola coche")
+        let copy = CPImageView(image: image)
+        copy.frame = frame
+        copy.contentMode = copy.contentMode
+        return copy
     }
     
+    required public init?(coder aDecoder: NSCoder) {
+        
+        parentViewController = nil
+        super.init(coder: aDecoder)
+        isUserInteractionEnabled = true
+    }
+    
+    override public func layoutSubviews() {
+        
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(showFullImage))
+        addGestureRecognizer(tapGesture)
+    }
+    
+    override public init(image: UIImage?) {
+        
+        parentViewController = nil
+        super.init(image: image)        
+        isUserInteractionEnabled = true
+    }
+    
+    @IBAction func showFullImage() {
+        
+        guard let parentViewController = parentViewController else {
+            return
+        }
+        
+        isHidden = true
+        
+        let fullImageVC = FullScreenViewControlller(self)
+        parentViewController.present(fullImageVC, animated: true, completion: nil)
+    }
+
 }
